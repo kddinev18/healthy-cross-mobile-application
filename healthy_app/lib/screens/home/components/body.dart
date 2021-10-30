393ccf2c63ba7c0e3da1fd5ac9d82a9791014f2c
kddinev18 <kddinev18@codingburgas.bg>
2021-10-30 12:06:41 +0300

    Add header

--- healthy_app/lib/screens/home/components/body.dart
+++ healthy_app/lib/screens/home/components/body.dart
@@ -10,61 +10,76 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Column(
-      children: [
-        Container(
-          height: size.height * 0.2,
-          child: Stack(
-            children: [
-              Container(
-                height: size.height * 0.2 - 27,
-                decoration: const BoxDecoration(
-                    color: kPrimaryColor,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(36),
-                      bottomRight: Radius.circular(36),
-                    )),
-              ),
-              Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    alignment: Alignment.center,
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    height: 54,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                              offset: const Offset(0, 10),
-                              blurRadius: 50,
-                              color: kPrimaryColor.withOpacity(0.23))
-                        ]),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            decoration: InputDecoration(
-                              hintText: "Search",
-                              hintStyle: TextStyle(
-                                  color: kPrimaryColor.withOpacity(0.5)),
-                              enabledBorder: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                            ),
-                          ),
-                        ),
-                        SvgPicture.asset("assets/icons/search.svg"),
-                      ],
-                    ),
-                  ))
-            ],
+      children: [Header(size, context)],
+    );
+  }
+
+  Container Header(Size size, BuildContext context) {
+    return Container(
+      height: size.height * 0.2,
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                left: kDefaultPadding,
+                right: kDefaultPadding,
+                bottom: kDefaultPadding * 2),
+            height: size.height * 0.2 - 27,
+            decoration: const BoxDecoration(
+                color: kPrimaryColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(36),
+                  bottomRight: Radius.circular(36),
+                )),
+            child: Row(
+              children: [
+                Text(
+                  'Random Text',
+                  style: Theme.of(context).textTheme.headline5.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
           ),
-        )
-      ],
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                height: 54,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: const Offset(0, 10),
+                          blurRadius: 50,
+                          color: kPrimaryColor.withOpacity(0.23))
+                    ]),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          hintText: "Search",
+                          hintStyle:
+                              TextStyle(color: kPrimaryColor.withOpacity(0.5)),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    SvgPicture.asset("assets/icons/search.svg"),
+                  ],
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
