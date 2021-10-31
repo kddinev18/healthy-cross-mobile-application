6d8501a3eeebe5583196fea84f9538b2de0790d4
kddinev18 <kddinev18@codingburgas.bg>
2021-10-31 19:40:19 +0200

    Implement dynamic column

--- healthy_app/lib/screens/home/components/body.dart
+++ healthy_app/lib/screens/home/components/body.dart
@@ -3,65 +3,75 @@ import 'package:flutter/material.dart';
 import 'package:healthy_app/constants.dart';
 
 import 'header.dart';
+import 'title_with_underscore.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({Key key}) : super(key: key);
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  List<int> bottom = <int>[0];
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Header(size: size),
-          Row(
+        child: Column(
+      children: [
+        Header(size: size),
+        Padding(
+          padding: const EdgeInsets.only(
+              left: kDefaultPadding, right: kDefaultPadding),
+          child: Row(
             children: [
-              const Title(
+              const TitleWithUnderscore(
                 text: "Recommended",
               ),
-              TextButton(
-                style: const ButtonStyle(),
-                onPressed: () {},
-                child: const Text("Add"),
-              )
+              Spacer(),
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: kPrimaryColor,
+                child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      bottom.add(bottom.length);
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.add,
+                    size: 20,
+                  ),
+                  color: Colors.white,
+                ),
+              ),
             ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class Title extends StatelessWidget {
-  const Title({Key key, this.text}) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              margin: const EdgeInsets.only(right: kDefaultPadding / 4),
-              height: 7,
-              color: kPrimaryColor.withOpacity(0.2),
-            ),
-          )
-        ],
-      ),
-    );
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      color: Colors.blue[200],
+                      height: 100,
+                      child: Text('Item: ${bottom[index]}'),
+                    );
+                  },
+                  childCount: bottom.length - 1,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
